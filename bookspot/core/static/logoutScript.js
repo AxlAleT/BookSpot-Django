37755afdfc359ecb6bdc34eae9248ae92345c03a
Axel Torres <104744161+AxlAleT@users.noreply.github.com>
2025-02-07 16:36:09 -0600

    logout button added to the sidebar

---
+++ bookspot/core/static/logoutScript.js
@@ -0,0 +1,24 @@
+async function logout() {
+  if (confirm("¿Desea cerrar sesión?")) {
+      const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+
+        try {
+            const response = await fetch('/api/logout/', {
+                method: 'POST',
+                headers: {
+                    'X-CSRFToken': csrftoken,
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Error en la respuesta del servidor');
+            }
+
+        } catch (error) {
+            console.error('Error al iniciar sesión:', error);
+            alert(`Error al iniciar sesión: ${error.message}`);
+        }
+        window.location.href = "/";
+  }
+}
